9e32127745b0d98075dcb39b2794485662b88793
maxvantisio <brownmh3@appstate.edu>
2025-05-24 13:05:41 -0400

    fixed bugs, updaetd colors, implemented movement and text editing in all three sections

--- src/App.tsx
+++ src/App.tsx
@@ -8,105 +8,155 @@ function App() {
   const [todoItems, setTodoItems] = useState<string[]>([])
   const [inProgressItems, setInProgressItems] = useState<string[]>([])
   const [doneItems, setDoneItems] = useState<string[]>([])
-  const [editingIdx, setEditingIdx] = useState<number | null>(null)
+  //const [editingIdx, setEditingIdx] = useState<number | null>(null)
+  const [editing, setEditing] = useState<{ idx: number, section: number } | null>(null)
   const [editingValue, setEditingValue] = useState('')
 
-  function toClick() {
-    setTodoItems([...todoItems, 'New Task'])
+  function toClick(index: number) {
+    if (index === 1) {
+      setTodoItems([...todoItems, 'New Task']);
+    } else if (index === 2) {
+      setInProgressItems([...inProgressItems, 'New Task']);
+    } else if (index === 3) {
+      setDoneItems([...doneItems, 'New Task']);
+    }
+    
+    
   }
 
-  function handleItemClick(idx: number) {
-    setEditingIdx(idx)
-    setEditingValue(todoItems[idx])
+  /* Edit text of appropiate task
+   *
+   * Params:
+   * idx - index of item being edited
+   * section - section item is in (1-3)
+   */
+  function handleItemClick(idx: number, section: number) {
+    setEditing({idx, section})
+    if (section === 1) {
+      setEditingValue(todoItems[idx])
+    } else if (section === 2) {
+      setEditingValue(inProgressItems[idx])
+    } else if (section === 3) {
+      setEditingValue(doneItems[idx])
+    }
   }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setEditingValue(e.target.value)
   }
 
-  function handleInputBlur(idx: number) {
-    const updated = [...todoItems]
-    updated[idx] = editingValue
-    setTodoItems(updated)
-    setEditingIdx(null)
+  /* update the text of the correct item
+   *
+   * Params:
+   * idx - index of item being updated
+   * section - section item is in (1-3)
+   */
+  function handleInputBlur(idx: number, section: number) {
+
+    if (section === 1) {
+      const updated = [...todoItems];
+      updated[idx] = editingValue;
+      setTodoItems(updated);
+    } else if (section === 2) {
+      const updated = [...inProgressItems];
+      updated[idx] = editingValue;
+      setInProgressItems(updated);
+    } else if (section === 3) {
+      const updated = [...doneItems];
+      updated[idx] = editingValue;
+      setDoneItems(updated);
+    }
+    setEditing(null)
   }
 
-  function handleInputKeyDown(e: React.KeyboardEvent<HTMLInputElement>, idx: number) {
+  function handleInputKeyDown(e: React.KeyboardEvent<HTMLInputElement>, idx: number, section: number) {
     if (e.key === 'Enter') {
-      handleInputBlur(idx)
+      handleInputBlur(idx, section)
     }
   }
 
-  function moveToDone(idx: number) {
+  //move task from In Progress to Done
+  function moveToDone(idx: number, section: number) {
     const item = inProgressItems[idx];
     setInProgressItems(inProgressItems.filter((_, i) => i !== idx));
     setDoneItems([...doneItems, item]);
-    if (editingIdx === idx) {
-      setEditingIdx(null);
+    if (JSON.stringify(editing) === JSON.stringify({idx, section})) {
+      setEditing(null);
     }
   }
 
-  function moveToTodo(idx: number) {
+  /* move task from In Progress to To Do
+   *
+   * Params:
+   * idx - item to be moved
+   */
+  function moveToTodo(idx: number, section: number) {
     const item = inProgressItems[idx];
     setInProgressItems(inProgressItems.filter((_, i) => i !== idx));
     setTodoItems([...todoItems, item]);
-    if (editingIdx === idx) {
-      setEditingIdx(null)
+    if (JSON.stringify(editing) === JSON.stringify({idx, section})) {
+      setEditing(null);
     }
   }
 
-  function moveToInProgressTodo(idx: number) {
-    const item = todoItems[idx];
-    setTodoItems(todoItems.filter((_, i) => i !== idx))
-    setInProgressItems([...inProgressItems, item])
-    if (editingIdx === idx) {
-      setEditingIdx(null)
+  /* move task from To Do or Done to In Progress
+   * 
+   * Params:
+   * idx - index of item to be moved
+   * section - section item is being moved from
+   *     1 - To Do
+   *     3 - Done
+   */
+  function moveToInProgress(idx: number, section: number) {
+    if (section === 1) {
+      const item = todoItems[idx];
+      setTodoItems(todoItems.filter((_, i) => i !== idx))
+      setInProgressItems([...inProgressItems, item])
+    } else if (section === 3) {
+      const item = doneItems[idx];
+      setDoneItems(doneItems.filter((_, i) => i !== idx))
+      setInProgressItems([...inProgressItems, item])
+    }
+    
+    if (JSON.stringify(editing) === JSON.stringify({idx, section})) {
+      setEditing(null);
     }
   }
-
-  function moveToInProgressDone(idx: number) {
-    const item = doneItems[idx];
-    setDoneItems(doneItems.filter((_, i) => i !== idx))
-    setInProgressItems([...inProgressItems, item])
-    if (editingIdx === idx) {
-      setEditingIdx(null)
-    }
-  }
-
-  
 
   return (
     <>
       <div className='h-screen flex flex-col overflow-x-hidden max-w-full overflow-y-hidden'>
-        <h2 className='text-center mt-4 font-bold text-2xl m-4'>[insert project name here]</h2>
-        <div className="container flex-1 grid gap-2 px-2 grid-cols-3" style={{minHeight:0}}>
-          <div className="column flex flex-col">
-            <div className="colHeader bg-todo rounded-t flex items-center justify-center cursor-pointer" onClick={toClick}>
-              <h4 className='m-2 text-textColor inline-block select-none'>To Do</h4>
+        <h2 className='text-center mt-4 font-bold text-2xl m-4 text-headerText'>[insert project name here]</h2>
+        <div className="container flex-1 grid gap-4 px-2 grid-cols-3" style={{minHeight:0}}>
+          <div className="column flex flex-col border-4 border-todoDarker rounded-lg">
+            <div className="colHeader bg-todo rounded-t flex items-center justify-center cursor-pointer" onClick={() => toClick(1)}>
+              <h4 className='m-2 text-textColor inline-block select-none font-semibold text-xl'>To Do</h4>
             </div>
-            <div className='colContent flex-1 bg-bgGrey overflow-auto'>
+            <div className='colContent flex-1 bg-bgTodo rounded-b overflow-auto border-r-0 border-l-0 border-b-0 border-t-3 border-todoDarker'>
               <ul id='todo' className='pt-2'>
                 {todoItems.map((item, idx) => (
-                  <li className='bg-white border-todo border-3 text-textColor mt-2 ml-4 mr-4 p-2 flex justify-between items-center rounded' key={idx}>
+                  <li 
+                  className='bg-todo border-todoDarker border-3 mt-2 ml-4 mr-4 mb-2 p-2 flex justify-between items-center rounded font-semibold text-xl' 
+                  key={idx}>
                     <div className='flex-1 flex items-center justify-center'>
-                    {editingIdx === idx ? (
+                    {JSON.stringify(editing) === JSON.stringify({idx, section: 1}) ? (
                       <input
                         value={editingValue}
                         autoFocus
                         onChange={handleInputChange}
-                        onBlur={() => handleInputBlur(idx)}
-                        onKeyDown={e => handleInputKeyDown(e, idx)}
+                        onBlur={() => handleInputBlur(idx, 1)}
+                        onKeyDown={e => handleInputKeyDown(e, idx, 1)}
                         className='w-full'
                       />
                     ) : (
                       <span className='w-full text-center cursor-pointer'
-                      onClick={() => handleItemClick(idx)}>{item}
+                      onClick={() => handleItemClick(idx, 1)}>{item}
                       </span>
                     )}
                     </div>
                     <button 
-                    className='ml-2 px-2 py-1 bg-todo text-textColor rounded hover:bg-blue-700'
-                    onClick={() => moveToInProgressTodo(idx)}
+                    className='ml-2 px-2 py-1 bg-todoDarker text-bgTodo rounded hover:bg-todoDarkest hover:text-textTodoDarker cursor-pointer'
+                    onClick={() => moveToInProgress(idx, 1)}
                     tabIndex={-1}>
                     &#10095;</button>
                   </li>
@@ -114,28 +164,44 @@ function App() {
               </ul>
             </div>
           </div>
-          <div className="column flex flex-col">
-            <div className='colHeader bg-doing flex items-center rounded-t justify-center'>
-              <h4 className='m-2 text-textColor inline-block select-none'>In Progress</h4>
+          <div className="column flex flex-col border-4 border-doingDarker rounded-lg">
+            <div className='colHeader bg-doing flex items-center rounded-t justify-center cursor-pointer' onClick={() => toClick(2)}>
+              <h4 className='m-2 text-textColor inline-block select-none font-semibold text-xl'>In Progress</h4>
             </div>
-            <div className='colContent flex-1 bg-bgGrey overflow-auto'>
+            <div className='colContent flex-1 bg-bgDoing overflow-auto rounded-b border-r-0 border-l-0 border-b-0 border-t-3 border-doingDarker'>
               <ul id='inprogress' className='pt-2'>
                 
                 {inProgressItems.map((item, idx) => (
-                  
+                 
                   <li
-                    className='bg-white border-doing border-3 mt-2 ml-2 mr-2 p-2 flex items-center justify-center rounded'
+                    className='bg-doing border-doingDarker border-3 mt-2 ml-4 mr-4 mb-2 p-2 flex items-center justify-center rounded font-semibold text-xl'
                     key={idx}
                   >
+                    
                     <button 
-                    className='ml-2 px-2 py-1 bg-doing text-textColor rounded hover:bg-blue-700'
-                    onClick={() => moveToTodo(idx)}
+                    className='mr-2 px-2 py-1 bg-doingDarker text-bgDoing rounded hover:bg-doingDarkest hover:text-textDoingDarker cursor-pointer'
+                    onClick={() => moveToTodo(idx, 1)}
                     tabIndex={-1}>
                     &#10094;</button>
-                    <span className="w-full text-center">{item}</span>
+                    <div className='flex-1 flex items-center justify-center'>
+                    {JSON.stringify(editing) === JSON.stringify({idx, section: 2}) ? (
+                      <input
+                        value={editingValue}
+                        autoFocus
+                        onChange={handleInputChange}
+                        onBlur={() => handleInputBlur(idx, 2)}
+                        onKeyDown={e => handleInputKeyDown(e, idx, 2)}
+                        className='w-full'
+                      />
+                    ) : (
+                      <span className='w-full text-center cursor-pointer'
+                      onClick={() => handleItemClick(idx, 2)}>{item}
+                      </span>
+                    )}
+                    </div>
                   <button 
-                    className='ml-2 px-2 py-1 bg-doing text-textColor rounded hover:bg-blue-700'
-                    onClick={() => moveToDone(idx)}
+                    className='ml-2 px-2 py-1 bg-doingDarker text-bgDoing rounded hover:bg-doingDarkst hover:text-textDoingDarker cursor-pointer'
+                    onClick={() => moveToDone(idx, 2)}
                     tabIndex={-1}>
                     &#10095;</button>
                   </li>
@@ -143,25 +209,39 @@ function App() {
               </ul>
             </div>
           </div>
-          <div className="column flex flex-col">
-            <div className='colHeader bg-done flex items-center rounded-t justify-center'>
-              <h4 className='m-2 text-textColor inline-block select-none'>Done</h4>
+          <div className="column flex flex-col border-4 border-doneDarker rounded-lg">
+            <div className='colHeader bg-done flex items-center rounded-t justify-center cursor-pointer' onClick={() => toClick(3)}>
+              <h4 className='m-2 text-textColor inline-block select-none font-semibold text-xl'>Done</h4>
             </div>
-            <div className='colContent flex-1 bg-bgGrey overflow-auto'>
+            <div className='colContent flex-1 bg-bgDone overflow-auto rounded-b border-r-0 border-l-0 border-b-0 border-t-3 border-doneDarker'>
               <ul id='done' className='pt-2'>
                 
                 {doneItems.map((item, idx) => (
                   
                   <li
-                    className='bg-white border-done border-3 mt-2 ml-2 mr-2 p-2 flex items-center justify-center rounded'
+                    className='bg-done border-doneDarker border-3 mt-2 ml-2 mr-2 p-2 flex items-center justify-center rounded font-semibold text-xl'
                     key={idx}
                   >
                     <button 
-                    className='ml-2 px-2 py-1 bg-done text-textColor rounded hover:bg-blue-700'
-                    onClick={() => moveToInProgressDone(idx)}
+                    className='mr-2 px-2 py-1 bg-doneDarker text-bgDone rounded hover:bg-doneDarkest hover:text-textDoneDarker cursor-pointer'
+                    onClick={() => moveToInProgress(idx, 3)}
                     tabIndex={-1}>
                     &#10094;</button>
-                    <span className="w-full text-center">{item}</span>
+                    <div className='flex-1 flex items-center justify-center'>
+                        {JSON.stringify(editing) === JSON.stringify({idx, section: 3}) ? (
+                          <input
+                            value={editingValue}
+                            autoFocus
+                            onChange={handleInputChange}
+                            onBlur={() => handleInputBlur(idx, 3)}
+                            onKeyDown={e => handleInputKeyDown(e, idx, 3)}
+                            className='w-full'
+                          />
+                        ) : (
+                          <span className='w-full text-center cursor-pointer'
+                            onClick={() => handleItemClick(idx, 3)}>{item}</span>
+                        )}
+                    </div>
                   </li>
                 ))}
               </ul>
